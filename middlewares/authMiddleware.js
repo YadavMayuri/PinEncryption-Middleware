2852a8ae3ea48a21b09d4b285e230c4f671c3d9b
madhuri yadav <mayuriyadav54@gmail.com>
2023-06-13 00:06:23 +0530

    updates

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -5,7 +5,7 @@ import encrypt from "encryptjs";
 
 export const registrationCheck= async (req, res, next) => {   
     try {
-        const { name, email, password, number,address, panNumber } = req.body;
+        const { name, email, password, number,address, panNumber,pin } = req.body;
 
         if (!name) return res.send("Name is required - middleware");
         if (!email) return res.send("Email is requierd - middleware");
@@ -13,6 +13,8 @@ export const registrationCheck= async (req, res, next) => {
         if (!number) return res.send("Number is requierd - middleware");
         if (!address) return res.send("Address is requierd - middleware");
         if (!panNumber) return res.send("Pan card number is requierd - middleware");
+        if (!pin) return res.send("Pin is requierd - middleware");
+
 
         if (password.length < 8) {
             return res.send("Password length is less than 8 !")
